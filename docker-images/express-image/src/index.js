ffa4f55a3a3a0900513920ceb8c15cdce3ce2f9e
dbnsky <emmanuel.schmid@gmail.com>
2017-06-11 22:43:54 +0200

    new version of index.js

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -13,29 +13,6 @@ app.listen(3000, function(){
 });
 
 function generateStudents(){
-	var numberOfStudents = chance.integer({
-		min: 0,
-		max: 10
-	});
-	console.log(numberOfStudents);
-	var students = [];
-	for(var i = 0; i < numberOfStudents; i++) {
-		var gender = chance.gender();
-		var birthYear = chance.year({
-			min: 1986,
-			max: 1996
-		});
-		students.push({
-			firstName: chance.first({
-				gender: gender
-			}),
-			lastName: chance.last(),
-			gender: gender,
-			birthday: chance.birthday({
-				year: birthYear
-			})
-		});
-	};
-	console.log(students);
-	return students;		
+
+	return chance.cc_types();		
 }
